8c0f8694c5f15262a79e8068961d4486ace446d6
MH <iu7726@naver.com>
2021-04-06 15:31:42 +0900

    몽고DB 접속 정보 로컬 파일로 수정

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./Models/User");
 
 //application/x-www-form-urlencoded
@@ -13,7 +15,7 @@ app.use(bodyParser.json());
 
 //DB Connect
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://dbUser:min472315@boilerplate.xsr8b.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI ,{
 	useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("Mongo DB Connected..."))
 	.catch(err => console.log(err))
